58df6b8cce3d8df984a518e1811828448092e6e2
Yousef Samet <yoosefsamet@gmail.com>
2026-06-07 12:18:01 +0330

    [fix] create building mechanism for netvanguard module

---
+++ Kbuild
@@ -0,0 +1,3 @@
+# Tell Kbuild to compile src/kernel/kernel.c into NetVanguard.ko
+obj-m += NetVanguard.o
+NetVanguard-objs := src/kernel/kernel.o
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -16,13 +16,3 @@ static void __exit testmod_exit(void)
 
 module_init(testmod_init);
 module_exit(testmod_exit);
-
-PWD := $(shell pwd)
-HEADERS := /lib/modules/$(shell uname -r)/build/
-obj-m := testmod.o
-
-modules:
-	make -C $(HEADERS) M=$(PWD) modules
-
-clean:
-	make -C $(HEADERS) M=$(PWD) clean
