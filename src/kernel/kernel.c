f447ee179e391753ee92c6511186c1743f9282ca
Yousef Samet <yoosefsamet@gmail.com>
2026-06-08 00:07:31 +0330

    [test] make simple test code for create simple TCP connection - add c_cpp_properties for intellisense in vscode - update configure.ac and Makefile.am for build tests directory - implement first test code for transfer one-shot TCP connection ( for test betfilter hooks )

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,18 +1,69 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
 
-MODULE_LICENSE("Dual MIT/GPL");
+MODULE_LICENSE("GPL");
 
-static int __init testmod_init(void)
+static struct nf_hook_ops *nfho = NULL;
+
+static unsigned int hfunc( void *priv, struct sk_buff *skb, const struct nf_hook_state *state )
 {
-    printk(KERN_INFO "Hi there!\n");
+	( void )priv;
+	struct iphdr  *iph;
+	struct udphdr *udph;
+
+	if ( skb && state )
+	{
+		iph = ip_hdr( skb );
+
+		if ( iph->protocol == IPPROTO_UDP )
+		{
+			udph = udp_hdr( skb );
+			if ( ntohs(udph->dest) == 53 )
+			{
+				return NF_ACCEPT;
+			}
+		}
+		else if ( iph->protocol == IPPROTO_TCP )
+		{
+			return NF_ACCEPT;
+		}
+	}
+	else
+	{
+		return NF_ACCEPT;
+	}
+	
+	return NF_DROP;
+}
+
+static int __init LKM_init(void)
+{
+    int err;
+	nfho = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
+	
+	nfho->hook 	= (nf_hookfn*)hfunc;		/* hook function */
+	nfho->hooknum 	= NF_INET_PRE_ROUTING;		/* received packets */
+	nfho->pf 	= PF_INET;			/* IPv4 */
+	nfho->priority 	= NF_IP_PRI_FIRST;		/* max hook priority */
+	
+	err = nf_register_net_hook(&init_net, nfho);
+	if ( err < 0 )
+		return err;
+
     return 0;
 }
 
-static void __exit testmod_exit(void)
+static void __exit LKM_exit(void)
 {
-    printk(KERN_INFO "Exit!\n");
+	nf_unregister_net_hook(&init_net, nfho);
+	kfree(nfho);
 }
 
-module_init(testmod_init);
-module_exit(testmod_exit);
+module_init(LKM_init);
+module_exit(LKM_exit);
---
+++ tests/tcp_simple_connection/src/tcp_handler.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include "tcp_handler.h"
+
+ssize_t tcp_socket_read( struct socket_config_t* socket_config,
+                     char*                   buffer,
+                     const size_t                  buffer_size )
+{
+    ssize_t read_byte;
+
+    read_byte = recv( socket_config->client.client_fd, buffer, buffer_size, 0 );
+    if ( read_byte < buffer_size )
+    {
+        perror("read()");
+        close( socket_config->socket_fd );
+        close( socket_config->client.client_fd );
+        return -1;
+    }
+
+    return read_byte;
+}
+
+ssize_t tcp_socket_send( struct socket_config_t* socket_config,
+                     const char*             message,
+                     size_t                  message_len)
+{
+
+    ssize_t send_byte = send( socket_config->socket_fd, message, message_len, 0 );
+    if ( send_byte < 15 )
+    {
+        perror("send()");
+        close( socket_config->socket_fd );
+        return -1;
+    }
+
+    return send_byte;
+}
+
+static int tcp_socket_listening( struct socket_config_t* socket_config, int max_connections )
+{
+    int ret;
+
+    ret = listen( socket_config->socket_fd, max_connections );
+    if ( ret < 0 )
+    {
+        perror("listen()");
+        close( socket_config->socket_fd );
+        return -1;
+    }
+
+    socket_config->client.client_fd = accept( socket_config->socket_fd,
+                                              ( struct sockaddr* )&socket_config->client.addr,
+                                              &socket_config->client.length );
+    if ( socket_config->client.client_fd < 0 )
+    {
+        perror("accept()");
+        close( socket_config->socket_fd );
+        close( socket_config->client.client_fd );
+        return -1;
+    }
+
+    return 0;
+}
+
+
+static int tcp_socket_connecting( struct socket_config_t* socket_config,
+                                  const char*             ip_addr,
+                                  const uint16_t          port_addr )
+{
+    int ret;
+
+    socket_config->server.addr.sin_family = AF_INET;
+    ret = inet_pton( AF_INET, ip_addr, &socket_config->server.addr.sin_addr.s_addr );
+    if ( ret < 0 )
+    {
+        perror("inet_pton()");
+        return -1;
+    }
+    socket_config->server.addr.sin_port = htons( port_addr );
+    socket_config->server.length = sizeof( struct sockaddr_in );
+
+
+    ret = connect( socket_config->socket_fd,
+                   (const struct sockaddr*)&socket_config->server.addr,
+                   socket_config->server.length );
+    if ( ret < 0 )
+    {
+        perror("connect()");
+        close( socket_config->socket_fd );
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int tcp_socket_init( struct socket_config_t* socket_config,
+                     enum   socket_type_t    socket_type,
+                     const char*             ip_addr,
+                     const uint16_t          port_addr)
+{
+    int    ret;
+    size_t ip_addr_len;
+
+    ip_addr_len = strlen( ip_addr );
+    if ( ip_addr_len > 15 ){return -1;}
+
+    socket_config->addr.sin_family = AF_INET;
+    ret = inet_pton( AF_INET, ip_addr, &socket_config->addr.sin_addr.s_addr );
+    if ( ret < 0 )
+    {
+        perror("inet_pton()");
+        return -1;
+    }
+    socket_config->addr.sin_port = htons( port_addr );
+    socket_config->length        = sizeof( struct sockaddr_in );
+
+    socket_config->socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( socket_config->socket_fd < 0 )
+    {
+        perror("socket()");
+        close( socket_config->socket_fd );
+        return -1;
+    }
+
+    ret = bind( socket_config->socket_fd,
+               (const struct sockaddr*)&socket_config->addr,
+                socket_config->length );
+    if ( ret < 0 )
+    {
+        perror("bind()");
+        close( socket_config->socket_fd );
+        return -1;
+    }
+
+    switch ( socket_type )
+    {
+    case SERVER_SIDE:
+    {
+        ret = tcp_socket_listening( socket_config, 10 );
+        break;
+    }
+    case CLIENT_SIDE:
+    {
+        ret = tcp_socket_connecting( socket_config, "127.0.1.10", 2323 );
+        break;
+    }
+    default:
+        printf("server_type invalid\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int tcp_socket_close( struct socket_config_t* socket_config,
+                      enum   socket_type_t    socket_type )
+{
+    switch ( socket_type )
+    {
+    case SERVER_SIDE:
+    {
+        close( socket_config->socket_fd );
+        close( socket_config->client.client_fd );
+        break;
+    }
+    case CLIENT_SIDE:
+    {
+        close( socket_config->socket_fd );
+        break;
+    }
+    default:
+        printf("server_type invalid\n");
+        return -1;
+    }
+
+    return 0;
+}
